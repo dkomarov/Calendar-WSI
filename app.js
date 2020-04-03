3a556c68505ae287467d39d6116dd50a460eda39
Jimmy Tran <tranj46@gmail.com>
2020-04-02 19:23:22 -0500

    Separate db connection into its' own file and call file in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose = require('mongoose');
+var mongodb = require('./lib/connect');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var authRoutes = require('./routes/auth-routes');
@@ -15,15 +15,8 @@ const keys = require('./config/keys');
 var app = express();
 const passport = require('passport');
 
-// mongoDB
-mongoose.connect('mongodb+srv://itmd567:'+process.env.MONGODB_PW+'@567websystems-qgpxm.azure.mongodb.net/test?retryWrites=true&w=majority', 
-  {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-  }  
-)
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Error: ', err));
+// mongoDB connection
+mongodb.dbConnect();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
