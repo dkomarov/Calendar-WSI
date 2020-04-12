57f15366f27180494ef6f027b2450f8ec6cd4fcb
dkomarov <dkomarov@hawk.iit.edu>
2020-04-11 22:54:34 -0500

    write first test for insert events function

---
+++ test/gcal-test.js
@@ -0,0 +1,22 @@
+const assert = require('chai').assert;
+//const assert = require('assert');
+
+const gcalFunction = require('../lib/gcalendar');
+
+const calendarData =  {
+  _id: '5e928d5da2f4db9e1a413205',
+  summary: 'event5',
+  location: 'loc',
+  description: 'desc',
+  start: '2020-04-12T06:00:00.000Z',
+  end: '2020-04-12T07:00:00.000Z',
+  recurrence: '3',
+  attendees: 'mail@mail.co',
+  reminders: 'none'
+}
+
+describe('insertEvent()', function() {
+  it('should return an inserted event object', function(){
+    assert.isObject(gcalFunction.insEvent(calendarData), 'object');
+  });
+});
