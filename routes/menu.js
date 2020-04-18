28f8c37dee8880388b854f8fdddc427864c5876a
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-04-18 16:38:55 -0500

    revert the last merge

--- routes/menu.js
+++ routes/menu.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 /* GET menu page. */
-router.get('/', function(req, res) {
+router.get('/', function(req, res, next) {
   res.render('menu', {user:req.user});
 });
 
