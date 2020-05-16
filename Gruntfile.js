1886d3a699c8d6c905c26b10f2d55959d1fa676e
dkomarov <dkomarov@hawk.iit.edu>
2020-05-15 22:18:08 -0500

    start template grunt file
    should be used for running repetitive tasks, eventually

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt){
+
+  //Configuration
+  grunt.initConfig({
+    //pass in options to plugins, references to files etc.
+    concat: {
+      js: {
+
+      }
+    }
+  });
+
+  // Register tasks
+  grunt.registerTask({})
+}
