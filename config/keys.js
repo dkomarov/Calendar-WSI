2504e1da5653f395014cec29ff23a14d76318024
dkomarov <dkomarov@hawk.iit.edu>
2020-05-29 22:18:34 -0500

    update connection info

--- config/keys.js
+++ config/keys.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   google: {
-    clientID: '896648827518-su9gq8nur635305vpfscpv4qg9qhhcld.apps.googleusercontent.com',
-    clientSecret: 'wKEJK_SlQRFpSqTHMv67--dM'
+    clientID: '721662891407-9tbtrvikhvv4rg3c9dnec4rlu0rbrvq4.apps.googleusercontent.com',
+    clientSecret: 'nKyscxluQjHUxWsIvAqqZbt6'
   },
   session: {
     cookieKey: 'websystemjustdoitgroup'
