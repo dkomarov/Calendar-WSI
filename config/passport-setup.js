e69264755ebb8cc044d8b2c26bb2d28855cdaf69
dkomarov <dkomarov@hawk.iit.edu>
2022-03-11 11:38:02 -0600

    adjust redirect urls (passport)

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,34 +1,35 @@
 /** Passport.js configuration setup module.
  * @module config/passport-setup
  */
-'use strict';
+"use strict";
+
+const env = process.env.NODE_ENV;
 
 /** Require module for passport js.
  * @requires passport
  */
-const passport = require('passport');
-
+const passport = require("passport");
 /** Require module for passports google authentication.
  * @requires passport-google-oauth20
  */
-const GoogleStrategy = require('passport-google-oauth20');
+const GoogleStrategy = require("passport-google-oauth20");
 
 /** Require module for keys file.
  * @requires keys
  */
-const keys = require('./keys');
+const keys = require("./keys");
 
 /** Require module for user model file.
  * @requires user-model
  */
-const User = require('../models/user-model');
+const User = require("../models/user-model");
 
 /** Passport serialize user function.
  * @function serializeUser
  * @param {object} user - User object
  * @param {object} done - Done object
  */
-passport.serializeUser((user, done)=> {
+passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
@@ -37,9 +38,9 @@ passport.serializeUser((user, done)=> {
  * @param {object} id - User ID object
  * @param {object} done - Done object
  */
-passport.deserializeUser((id,done)=> {
-  User.findById(id).then((user)=> {
-    done(null,user);
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
   });
 });
 
@@ -47,38 +48,52 @@ passport.deserializeUser((id,done)=> {
  * @function use
  */
 passport.use(
-  new GoogleStrategy({
-    /** Options for the google strategies. */
-    callbackURL: '/auth/google/redirect',
-    clientID: keys.google.clientID,
-    clientSecret: keys.google.clientSecret,
-    }, (accessToken, refreshToken, profile, done)=> {
-        /** Passport callback function. */
-        User.findOne({
-            googleId: profile.id
-        }).then((currentUser) => {
-            if(currentUser){
-                /** User exists. */
-                User.updateOne({googleId: profile.id},{googleToken : accessToken},function(err, obj) {
-                    if (err){
-                      throw err;
-                    }else{
-                      console.log("Token updated");
-                    }});
-                console.log('user already exists', currentUser);
-                done(null,currentUser);
-            }else{
-                /** Create User. */
-                new User({
-                    userName: profile.displayName,
-                    googleId: profile.id,
-                    googleToken: accessToken,
-                    googleRefresh: refreshToken
-                }).save().then((newUser) => {
-                    console.log('new user was created' +newUser);
-                    done(null, newUser);
-                });
+  new GoogleStrategy(
+    {
+      /** Options for the google strategies. */
+      callbackURL:
+        process.env.NODE_ENV == "development"
+          ? "http://localhost:3000/auth/google/redirect"
+          : "https://calendar-wsi.herokuapp.com/auth/google/redirect",
+      // callbackURL: "/auth/google/redirect",
+      clientID: keys.google.clientID,
+      clientSecret: keys.google.clientSecret,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      /** Passport callback function. */
+      User.findOne({
+        googleId: profile.id,
+      }).then((currentUser) => {
+        if (currentUser) {
+          /** User exists. */
+          User.updateOne(
+            { googleId: profile.id },
+            { googleToken: accessToken },
+            function (err, obj) {
+              if (err) {
+                throw err;
+              } else {
+                console.log("Token updated");
+              }
             }
-        });
-      })
-)
+          );
+          console.log("user already exists", currentUser);
+          done(null, currentUser);
+        } else {
+          /** Create User. */
+          new User({
+            userName: profile.displayName,
+            googleId: profile.id,
+            googleToken: accessToken,
+            googleRefresh: refreshToken,
+          })
+            .save()
+            .then((newUser) => {
+              console.log("new user was created" + newUser);
+              done(null, newUser);
+            });
+        }
+      });
+    }
+  )
+);
